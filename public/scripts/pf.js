934240ac70731d61a97aebbf289664e6fbea0b6f
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-08-19 14:58:48 +0200

    Squared Menu

--- public/scripts/pf.js
+++ public/scripts/pf.js
@@ -5,14 +5,38 @@ var PF = (function (){
 	var centerButtonClicked = false;
 	var centerButtonHovered = false;
 	var items;
+	var myName;
+	var subCircle;
+
+	var itemColors = [
+		"#E57373",
+		"#64B5F6",
+		"#FFA726",
+		"#F06292"
+	]
 
 	self.initialize = function() {
 		centerButton = document.getElementById("centerButton");
 		items = document.getElementsByClassName("menuItem");
+		myName = document.getElementById("myName");
+		subCircle = document.getElementById("subCircle");
+
+		for(var i=0;i<items.length;i++) {
+			(function(index) {
+				items[index].addEventListener("mouseover", function() {
+					document.body.style.backgroundColor = itemColors[index];
+				});
+
+				items[index].addEventListener("mouseout", function () {
+					document.body.style.backgroundColor = "rgba(239, 108, 0, 0.4)";
+				});
+			})(i)
+		}
 
 		centerButton.addEventListener("mouseover", function() {
 			centerButtonHovered = true;
-			document.body.style.backgroundColor = "rgba(239, 108, 0, 0.4)";
+			if(!centerButtonClicked)
+				document.body.style.backgroundColor = "rgba(239, 108, 0, 0.4)";
 		});
 
 		centerButton.addEventListener("mouseout", function() {
@@ -36,6 +60,12 @@ var PF = (function (){
 	function showMenuItems() {
 		centerButton.className = "bigCircle active black";
 		centerButton.style.webkitTransform = "";
+
+		myName.innerHTML = "Baptiste Maréchaux";
+		myName.className = "myName active";
+
+		subCircle.style.visibility = "hidden";
+		
 		document.body.style.backgroundColor = "rgba(239, 108, 0, 0.4)";
 		for(var i=0;i<items.length;i++) {
 			items[i].className = "menuItem visible";
@@ -45,6 +75,11 @@ var PF = (function (){
 	function hideMenuItems() {
 		centerButton.className = "bigCircle inactive";
 		centerButtonHovered = false;
+		myName.innerHTML = "BM";
+		myName.className = "myName inactive";
+
+		subCircle.style.visibility = "visible";
+
 		document.body.style.backgroundColor = "rgba(255, 255, 255, 0)";
 		for(var i=0;i<items.length;i++) {
 			items[i].className = "menuItem hidden";
