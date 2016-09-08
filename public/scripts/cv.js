7b84737359504c4e5dbddf2ae2cee1de861c1a90
Baptiste Maréchaux <baptiste.marechaux@gmail.com>
2016-09-08 23:13:32 +0200

    No spinning around for 4 first buttons

---
+++ public/scripts/cv.js
@@ -0,0 +1,15 @@
+var BaptisteCV = (function() {
+    var self = {};
+    var activated = false;
+
+    var body = document.body;
+    self.init = function() {
+        activated = true;
+        
+    };
+
+    return self;
+})();
+
+
+BaptisteCV.init();
